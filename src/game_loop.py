297ba8a014a64a33f3a2fbeaf4598f37a86cc1fa
Grégory Châtel <chatel.gregory@gmail.com>
2018-11-29 23:20:37 +0100

    Full game working with monte carlo ai.

--- src/game_loop.py
+++ src/game_loop.py
@@ -1,19 +1,41 @@
 from game_engine import GameState
-from monte_carlo import play_to_the_end
+from monte_carlo import find_best_next_move
 
-def game_loop():
+def game_loop_human_vs_ai(n = 20000):
     state = GameState()
 
     while not state.is_finished():
+        if state.get_player() == 0:
+            print(state)
+            print(*(f'{a} {b} {c} {d}' for a, b, c, d in state.get_possible_next_move()), sep = ', ')
+            grid_x, grid_y, cell_x, cell_y = map(int, input().split())
+            state.play(grid_x, grid_y, cell_x, cell_y)
+            print(state)
+        else:
+            next_ai_move = find_best_next_move(state, n)
+            state.play(*next_ai_move)
+
+    print(state)
+    print(f'Winner: {state.winner}')
+
+def game_loop_ai_vs_ai(n = 20000):
+    state = GameState()
+
+    while not state.is_finished():
+        # if state.get_player() == 0:
+        #     print(state)
+        #     print(*(f'{a} {b} {c} {d}' for a, b, c, d in state.get_possible_next_move()), sep = ', ')
+        #     grid_x, grid_y, cell_x, cell_y = map(int, input().split())
+        #     state.play(grid_x, grid_y, cell_x, cell_y)
+        #     print(state)
+        # else:
         print(state)
-        print(state.get_possible_next_move())
-        grid_x, grid_y, cell_x, cell_y = map(int, input().split())
-        state.play(grid_x, grid_y, cell_x, cell_y)
+        next_ai_move = find_best_next_move(state, n)
+        state.play(*next_ai_move)
 
     print(state)
     print(f'Winner: {state.winner}')
 
 if __name__ == '__main__':
-    # game_loop()
-    state = GameState()
-    play_to_the_end(state)
+    # game_loop_human_vs_ai()
+    game_loop_ai_vs_ai()
--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -1,4 +1,6 @@
 from game_engine import GameState
+
+from multiprocessing import Pool, cpu_count
 from random import sample
 from copy import deepcopy
 from collections import Counter
@@ -12,8 +14,46 @@ def play_to_the_end(state):
 
     return state.get_winner()
 
+def evaluate_position(state, n):
+    pool    = Pool(cpu_count())
+    results = pool.map(play_to_the_end, [state] * n)
+    pool.close()
+    counter = Counter(results)
+
+    return {
+        0    : counter[0] / n,
+        1    : counter[1] / n,
+        None : counter[None] / n
+    }
+
+def find_best_next_move(state, n):
+    possible_moves    = list(state.get_possible_next_move())
+    n_next_move       = len(possible_moves)
+    run_by_move       = n // n_next_move
+    player            = state.get_player()
+    move_to_win_proba = {}
+
+    for move in possible_moves:
+        move_state = deepcopy(state)
+        move_state.play(*move)
+        proba = evaluate_position(move_state, run_by_move)
+        move_to_win_proba[move] = proba[player]
+
+    result = sorted(move_to_win_proba.items(), key = lambda x: x[1], reverse = True)
+    print(f'Number of game run by potential move: {run_by_move}')
+    print(
+        *(f'{xg} {yg} {xc} {yc} -> {100 * proba:5.2f}%' for (xg, yg, xc, yc), proba in result),
+        sep = '\n'
+    )
+
+    return result[0][0]
+
 if __name__ == '__main__':
-    n       = 20000
+    n       = 50000
     state   = GameState()
-    results = [play_to_the_end(state) for _ in range(n)]
-    print(Counter(results))
+    # for i in range(30):
+    #     win_0, win_1, draw = evaluate_position(state, n)
+    #     print(f'[{i:2d}] {100 * win_0:5.2f}% {100 * win_1:5.2f}% {100 * draw:5.2f}%')
+    state.play(1, 1, 1, 1)
+    result = find_best_next_move(state, n)
+    print(result)
