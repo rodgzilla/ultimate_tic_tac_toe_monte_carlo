030df2a33bcda050b182b711b9e3fcc3e0243888
Grégory Châtel <chatel.gregory@gmail.com>
2018-11-29 19:40:13 +0100

    All test failing.

--- src/game_engine.py
+++ src/game_engine.py
@@ -11,16 +11,15 @@ class GameState():
                 ] for _ in range(3)   # line of big grid
             ] for _ in range(3)       # big grid
         ]
-        self.board = [[[[None, None, None], [None, None, None], [0, None, None]],
-                       1,
-                       [[None, None, None], [None, None, None], [None, 1, None]]],
-                      [[[0, None, None], [None, None, None], [None, None, None]],
-                       [[None, None, None], [None, None, None], [None, None, None]],
-                       0],
-                      [[[None, None, None], [None, None, None], [None, None, None]],
-                       [[None, None, None], [None, None, None], [None, None, None]],
-                       [[0, None, None], [None, None, None], [None, None, None]]]]
-
+        # self.board = [[[[None, None, None], [None, None, None], [0, None, None]],
+        #                1,
+        #                [[None, None, None], [None, None, None], [None, 1, None]]],
+        #               [[[0, None, None], [None, None, None], [None, None, None]],
+        #                [[None, None, None], [None, None, None], [None, None, None]],
+        #                0],
+        #               [[[None, None, None], [None, None, None], [None, None, None]],
+        #                [[None, None, None], [None, None, None], [None, None, None]],
+        #                [[0, None, None], [None, None, None], [None, None, None]]]]
 
     def play(self, grid_x, grid_y, cell_x, cell_y):
         pass
@@ -32,7 +31,7 @@ class GameState():
         pass
 
     def get_possible_next_move(self):
-        pass
+        return []
 
     def is_finished(self):
         pass
@@ -65,17 +64,26 @@ class GameState():
         return '\n'.join(repr_lines)
 
 class TestGameEngine(unittest.TestCase):
-    def test_init(self):
-        pass
-
     def test_play(self):
-        pass
+        state = GameState()
+        state.play(0, 0, 0, 0)
+        with self.assertRaises(ValueError):
+            state.play(0, 0, 0, 0)
 
-    def test_get_global_board(self):
-        pass
+        moves = [
+            (0, 0, 0, 1),
+            (0, 1, 0, 0),
+            (0, 0, 1, 1),
+            (1, 1, 0, 0),
+            (0, 0, 2, 1),
+            (2, 1, 0, 0),
+        ]
 
-    def test_get_local_board(self):
-        pass
+        for move in moves:
+            state.play(*move)
+
+        with self.assertRaises(ValueError):
+            state.play(0, 0, 2, 2)
 
     def test_get_possible_next_move_initial(self):
         state          = GameState()
@@ -89,12 +97,82 @@ class TestGameEngine(unittest.TestCase):
         self.assertEqual(set(state.get_possible_next_move()), possible_moves)
 
     def test_get_possible_next_move_single_grid(self):
-        pass
+        state = GameState()
+        state.play(0, 0, 0, 0)
+        possible_moves = {
+            (0, 0, x, y)
+            for x in range(3)
+            for y in range(3)
+            if not (x == 0 and y == 0)
+        }
+        self.assertEqual(
+            set(state.get_possible_next_move()),
+            possible_moves
+        )
+        state.play(0, 0, 0, 1)
+        possible_moves = {
+            (0, 1, x, y)
+            for x in range(3)
+            for y in range(3)
+        }
+        self.assertEqual(
+            set(state.get_possible_next_move()),
+            possible_moves
+        )
 
     def test_get_possible_next_move_multi_grid(self):
-        pass
+        # We fill the top left cell as quickly as possible and then
+        # play a top left cell in a small grid to allow the opponent
+        # to play anywhere on the board.
+        moves = [
+            (0, 0, 0, 0),
+            (0, 0, 0, 1),
+            (0, 1, 0, 0),
+            (0, 0, 1, 1),
+            (1, 1, 0, 0),
+            (0, 0, 2, 1),
+            (2, 1, 0, 0),
+        ]
+
+        # Create the board and play the moves.
+        state = GameState()
+        for move in moves:
+            state.play(*move)
+
+        all_moves = {
+            (grid_x, grid_y, cell_x, cell_y)
+            for grid_x in range(3)
+            for grid_y in range(3)
+            for cell_x in range(3)
+            for cell_y in range(3)
+        }
+        impossible_moves = {
+            # cells in the top left cell
+            (0, 0, 0, 0),
+            (0, 0, 0, 1),
+            (0, 0, 0, 2),
+            (0, 0, 1, 0),
+            (0, 0, 1, 1),
+            (0, 0, 1, 2),
+            (0, 0, 2, 0),
+            (0, 0, 2, 1),
+            (0, 0, 2, 2),
+            # cells already filled
+            (0, 1, 0, 0),
+            (1, 1, 0, 0),
+            (2, 1, 0, 0),
+        }
+
+        possible_moves = all_moves - impossible_moves
+        self.assertEqual(
+            set(state.get_possible_next_move()),
+            possible_moves
+        )
 
     def test_is_finished(self):
+        # We finish the game quickly and check during the completion
+        # and after the last move whether the game state indicates
+        # finished or not.
         state = GameState()
         self.assertFalse(state.is_finished())
         moves = [
@@ -118,6 +196,7 @@ class TestGameEngine(unittest.TestCase):
 
         for move in moves[:6]:
             state.play(*move)
+
         self.assertFalse(state.is_finished())
 
         for move in moves[6:]:
